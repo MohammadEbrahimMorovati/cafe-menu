86f15d19a74f17d96a9501f862c7dea4a2bdf80a
Moro <morovati734@gmail.com>
2025-10-07 23:25:05 +0330

    change vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   tailwind: {
+//     theme: {
+//       extend: {
+//         fontFamily: {
+//           body: "var(--font-body)",
+//           title: "var(--font-title)",
+//         },
+//       },
+//     },
+//   },
+//   server: {
+//     host: true,   // معادل 0.0.0.0
+//     port: 5173,   // چون 5173 پر بود
+//     proxy: {
+//       "/api": {
+//         target: "http://127.0.0.1:8000",
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// ⚙️ تنظیمات نهایی برای build در Vercel
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   tailwind: {
@@ -15,15 +44,7 @@ export default defineConfig({
       },
     },
   },
-  server: {
-    host: true,   // معادل 0.0.0.0
-    port: 5173,   // چون 5173 پر بود
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:8000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+  build: {
+    outDir: "dist", // 📦 خروجی بیلد برای Vercel
   },
 });
